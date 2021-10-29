67a9f96b8836a676e7ddfa11e54e6e02e1999b79
Karthik Poomalraj <karthik@poweredbyb.com>
2021-10-28 23:49:09 -0230

    Handling mongo and JWT errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,7 @@ const catchAsyncError = require('../middlewares/catchAsyncErrors')
 const sendToken = require('../utils/jwtToken')
 const sendEmail = require('../utils/sendEmail')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const crypto = require('crypto');
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -110,5 +111,37 @@ exports.forgotPassword = catchAsyncError(async(req, res, next) => {
         await user.save({ validateBeforeSave : false });
         return next(new ErrorHandler(error.message, 500))
     }
+
+})
+
+//Reset password => /api/v1/password/reset/:token
+
+exports.resetPassword = catchAsyncError(async(req, res, next) => {
+    //Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken, resetPasswordExpire: {
+            $gt: Date.now()
+        }
+    })
+
+    if(!user)
+    {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if(req.body.password !== req.body.confirmpassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    //Setup new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res);
 
 })
--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -33,9 +33,34 @@ module.exports = (err, req, res, next) => {
             error = new ErrorHandler(message, 400);
         }
 
+        
+        //Handling mongoose duplicate key errors
+        if(err.code === 11000)
+        {
+            console.log(err);
+            error = new ErrorHandler(err, 400);
+        }
+
+        //Handling wrong JWT error
+        if(err.name === 'JsonWebTokenError')
+        {
+            const message = 'JSON Web Token is invalid, please try again'
+            error = new ErrorHandler(message, 400);
+        }
+
+        //Handling expired JWT error
+        if(err.name === 'JsonWebTokenError')
+        {
+            const message = 'JSON Web Token is expired, please try again'
+            error = new ErrorHandler(message, 400);
+        }
+
         res.status(err.statusCode).json({
             success:false,
             error: error.message
         })
+
     }
+
+
 }
