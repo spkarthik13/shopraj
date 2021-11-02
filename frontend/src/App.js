6900ff2e04d05670349dd47a45e0733cc7fa3c16
Karthik Poomalraj <karthik@poweredbyb.com>
2021-11-01 21:43:06 -0230

    Added front end react functionality on homepage

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Header } from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import Home from "./components/Home";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header/>
+        <div className="container container-fluid">
+          <Route path= "/" component={Home} exact />
+        </div>
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
